cee091e2537fc8072f2e6b009f665dcac92ac509
Jim <jim.holmstroem@gmail.com>
2022-10-26 18:07:24 +0200

    feature: .clearCanvas + .canvasDimensions

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ npm install @nextml/far-canvas
 For example: translated `100'000'000px` away from the center (and a scaling of 1.5) and rendering the objects that far away:
 
 ### vanilla canvas exapmle at 0px translation
+
 <img
   src="static/reference-canvas.png"
   alt="vanilla canvas example"
@@ -18,6 +19,7 @@ For example: translated `100'000'000px` away from the center (and a scaling of 1
   style="display: inline-block; margin: 0 auto;">
 
 ### vanilla canvas example at 100Mpx translation
+
 <img
   src="static/vanilla-canvas.png"
   alt="vanilla canvas example"
@@ -25,6 +27,7 @@ For example: translated `100'000'000px` away from the center (and a scaling of 1
   style="display: inline-block; margin: 0 auto;">
 
 ### far canvas example at 100Mpx translation
+
 <img
   src="static/far-canvas.png"
   alt="far canvas example"
@@ -35,35 +38,46 @@ For example: translated `100'000'000px` away from the center (and a scaling of 1
 2. `lineWidth=8px` is not rendered correctly.
 
 ## usage
+
 ### Node
+
 ```javascript
 const { far } = require("../lib.cjs/index.js");
 
 const farAway = 100000000;
 const context = far(canvas, {y: -farAway, scale: 2}).getContext("2d");
 
+context.clearCanvas();
 context.fillRect(32, farAway + 16, 128, 128);
+
+context.canvas; // underlying canvas for which the default unit is pixels
+context.s; // coordinate system
+context.s.inv; // inverse coordinate system
+
 ...
 ```
 
 ### Web
+
 ```javascript
 const canvas = document.getElementById('far');
 
 const farAway = 100000000;
 const context = far.far(canvas, {y: -farAway, scale: 2}).getContext("2d");
 
-context.fillRect(32, farAway + 16, 128, 128);
 ...
 ```
 
 ## development
+
 ### run exampl
+
 ```bash
 npm run example
 ```
 
 ### update version
+
 ```bash
 npm version patch | minor | major
 ```
--- example/example.js
+++ example/example.js
@@ -1,98 +1,114 @@
 function getReferenceContext2d(element, transform) {
-    const context = element.getContext("2d");
-    context.scale(transform.scale, transform.scale);
-    context.translate(transform.x, transform.y);
+  const context = element.getContext("2d");
+  context.scale(transform.scale, transform.scale);
+  context.translate(transform.x, transform.y);
 
-    return context;
-};
-
-function getFarContext2d(element, transform) {
-    const context = far.far(element, transform).getContext("2d");
-
-    return context;
+  return context;
 }
 
-const referenceCanvas = document.getElementById('reference');
-const farCanvas = document.getElementById('far');
+function getFarContext2d(element, transform) {
+  const context = far.far(element, transform).getContext("2d");
 
-const image = {data: document.createElement('img'), width: 300, height: 150};
-const canvasDimensions = {width: 700, height: 1200};
+  return context;
+}
+
+const referenceCanvas = document.getElementById("reference");
+const farCanvas = document.getElementById("far");
+
+const image = { data: document.createElement("img"), width: 320, height: 164 };
+const canvasDimensions = { width: 700, height: 1200 };
 
 referenceCanvas.width = canvasDimensions.width;
 referenceCanvas.height = canvasDimensions.height;
 farCanvas.width = canvasDimensions.width;
 farCanvas.height = canvasDimensions.height;
 
-
 const scale = canvasDimensions.width / image.width;
 const focus = 10000; // 500000000 // breaks down in vanilla canvas
 
 const diff = -image.height * 0;
 
-const mkImage = ({x, y, image}) => ({ x , y, data: image.data, width: image.width, height: image.height});
+const mkImage = ({ x, y, image }) => ({
+  x,
+  y,
+  data: image.data,
+  width: image.width,
+  height: image.height,
+});
 
 const images = [
-    mkImage({ x: 0, y: focus - 1*image.height, image }),
-	mkImage({ x: 0, y: focus + 0*image.height, image }),
-    mkImage({ x: 0, y: focus + 1*image.height, image }),
-    mkImage({ x: 0, y: focus + 2*image.height, image }),
-    mkImage({ x: 0, y: focus + 3*image.height, image }),
-    mkImage({ x: 0, y: focus + 4*image.height, image }),
+  mkImage({ x: 0, y: focus - 1 * image.height, image }),
+  mkImage({ x: 0, y: focus + 0 * image.height, image }),
+  mkImage({ x: 0, y: focus + 1 * image.height, image }),
+  mkImage({ x: 0, y: focus + 2 * image.height, image }),
+  mkImage({ x: 0, y: focus + 3 * image.height, image }),
+  mkImage({ x: 0, y: focus + 4 * image.height, image }),
 ];
 
 const rectangles = [
-	{ x: 10, y: focus + 20, width: 200, height: 30 },
-    { x: 100, y: focus + 250, width: 200, height: 30 },
-    { x: -10, y: focus - 10, width: 200, height: 30 },
-    { x: 100, y: focus + 400, width: 200, height: 30 },
-    { x: 0, y: focus + 2 * image.height, width: image.width, height: image.height },
+  { x: 10, y: focus + 20, width: 200, height: 30 },
+  { x: 100, y: focus + 250, width: 200, height: 30 },
+  { x: -10, y: focus - 10, width: 200, height: 30 },
+  { x: 100, y: focus + 400, width: 200, height: 30 },
+  {
+    x: 0,
+    y: focus + 2 * image.height,
+    width: image.width,
+    height: image.height,
+  },
 ];
 
 const contextReference = getReferenceContext2d(
-	document.getElementById('reference'),
-    { x: 0, y: -focus - diff, scale: scale }
+  document.getElementById("reference"),
+  { x: 0, y: -focus - diff, scale: scale }
 );
-const contextFar = getFarContext2d(
-	document.getElementById('far'),
-    { x: 0, y: -focus - diff, scale: scale }
-);
+const contextFar = getFarContext2d(document.getElementById("far"), {
+  x: 0,
+  y: -focus - diff,
+  scale: scale,
+});
 
 image.data.onload = function () {
-	function render(ctx) {
-    	images.forEach(image => {
-            ctx.save();
+  function render(ctx) {
+    images.forEach((image) => {
+      ctx.save();
 
-            ctx.drawImage(image.data, image.x, image.y, image.width, image.height);
-            ctx.beginPath();
-            ctx.strokeStyle = "#803";
-            ctx.lineWidth = 1;
-            ctx.rect(image.x, image.y, image.width, image.height);
-            ctx.stroke();
+      ctx.drawImage(image.data, image.x, image.y, image.width, image.height);
+      ctx.beginPath();
+      ctx.strokeStyle = "#803";
+      ctx.lineWidth = 1;
+      ctx.rect(image.x, image.y, image.width, image.height);
+      ctx.stroke();
 
-            ctx.restore();
-        });
-        rectangles.forEach(rectangle => {
-            ctx.save();
+      ctx.restore();
+    });
+    rectangles.forEach((rectangle) => {
+      ctx.save();
 
-            ctx.fillStyle = "#CE0";
-            ctx.fillRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
-            ctx.strokeStyle = "#803";
-            ctx.lineWidth = 8;
-            ctx.beginPath();
-            ctx.moveTo(rectangle.x, rectangle.y);
-            ctx.lineTo(rectangle.x + rectangle.width, rectangle.y + rectangle.height);
-            ctx.stroke();
-            ctx.beginPath();
-            ctx.moveTo(rectangle.x + rectangle.width, rectangle.y);
-            ctx.lineTo(rectangle.x, rectangle.y + rectangle.height);
-            ctx.stroke();
+      ctx.save();
+      ctx.fillStyle = "#CE0";
+      ctx.fillRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+      ctx.restore();
 
-            ctx.restore();
-        });
-    }
+      ctx.save();
+      ctx.strokeStyle = "#803";
+      ctx.lineWidth = 8;
+      ctx.beginPath();
+      ctx.moveTo(rectangle.x, rectangle.y);
+      ctx.lineTo(rectangle.x + rectangle.width, rectangle.y + rectangle.height);
+      ctx.stroke();
+      ctx.beginPath();
+      ctx.moveTo(rectangle.x + rectangle.width, rectangle.y);
+      ctx.lineTo(rectangle.x, rectangle.y + rectangle.height);
+      ctx.stroke();
+      ctx.restore();
 
-    render(contextReference);
-    render(contextFar);
+      ctx.restore();
+    });
+  }
 
-}
-image.data.src = "https://docs.gimp.org/2.6/en/images/tutorials/quickie-jpeg-example.jpg";
+  render(contextReference);
+  render(contextFar);
+};
+image.data.src =
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Saturn_from_Cassini_Orbiter_%282004-10-06%29.jpg/320px-Saturn_from_Cassini_Orbiter_%282004-10-06%29.jpg";
